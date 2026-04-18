a3a96e6235269776015eb48634bcd627d6154b8f
Ryan Souza <ryanssebadelhe@gmail.com>
2026-04-18 17:42:13 -0300

    Finalizando modal

--- frontend/js/controllers/modal-controller.js
+++ frontend/js/controllers/modal-controller.js
@@ -1,4 +1,36 @@
 
+function State() {
+    this.container = null;
+    this.btnClose = null;    
+}
+
+const state = new State();
+
+
 export function init(){
-    console.log("Modal controller initialized");
+    state.container = document.querySelector("#modal-contact");
+    state.btnClose = state.container.querySelector("#modal-content-close");
+
+    state.btnClose.addEventListener("click", handleBtnCloseClick);
+    state.container.addEventListener("click", handleContainerClick);
+}
+
+export function showModal(){
+    state.container.classList.add("active");
+}
+
+export function closeModal(){
+    state.container.classList.remove("active");
+}
+
+function handleBtnCloseClick(event){
+    event.preventDefault();
+    closeModal();
+}
+
+function handleContainerClick(event){
+    event.preventDefault();
+    if(event.target === this){
+        closeModal();
+    }
 }
